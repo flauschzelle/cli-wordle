9cb4fac3c7210b338ebc6c85e99c74fbc150364e
Kirstin Rohwer <mail@metakiki.net>
2022-02-21 16:43:31 +0100

    convert input to uppercase, cut off too long inputs, display message for too short inputs

--- console_wordle.py
+++ console_wordle.py
@@ -3,13 +3,15 @@
 # A wordle implementation for unix terminals. 
 # This will not work correctly on Windows, sorry!
 
-solution = "wordl"
+solution = "WORDL"
+word_len = 5
 guesses = 0
 max_guesses = 6
 
 BGREEN = '\033[42m'
 BYELLOW = '\033[43m'
 BBLACK = '\033[40m'
+BGRAY = '\033[100m' 
 
 BLACK = '\033[30m'
 WHITE = '\033[37m'
@@ -18,7 +20,7 @@ ENDC = '\033[0m'
 BOLD = '\033[1m'
 
 print("Welcome to CONSOLE WORDLE!")
-print("Can you guess the secret word in", max_guesses, "or less tries?\n")
+print("Can you guess the secret", word_len, "letter word in", max_guesses, "or less tries?\n")
 
 def bold_colored_text(text: str, color: str) -> str:
 	ctext = f"{BOLD}{color}{text}{ENDC}"
@@ -26,9 +28,14 @@ def bold_colored_text(text: str, color: str) -> str:
 	
 while guesses < max_guesses:
 
-	guess = str(input())
-	
+	# truncate input string and convert to uppercase:
+	guess = str(input())[0:word_len].upper()
+
 	print("\x1B[F\x1B[2K", end="") # overwrite input line after entering
+
+	if len(guess) < word_len:
+		print(f"      {guess} is too short. Words must be {word_len} letters long!")
+		continue
 
 	print(bold_colored_text(guess, BGREEN))
 
