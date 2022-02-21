02a049e632258293108288e22993be8a94598e08
Kirstin Rohwer <mail@metakiki.net>
2022-02-21 14:59:23 +0100

    overwrite input lines, improved intro/win/lose messages

--- console_wordle.py
+++ console_wordle.py
@@ -1,23 +1,29 @@
 #!/usr/bin/env python3
 
+# A wordle implementation for unix terminals. 
+# This will not work correctly on Windows, sorry!
+
 solution = "wordl"
 guesses = 0
 max_guesses = 6
 
 print("Welcome to CONSOLE WORDLE!")
-print("Can you guess the secret word in", max_guesses, "or less tries?\n\n")
+print("Can you guess the secret word in", max_guesses, "or less tries?\n")
 
 while guesses < max_guesses:
 
 	guess = str(input())
+	
+	print("\x1B[F\x1B[2K", end="") #overwrite input line after entering
 	print(guess)
 
 	guesses += 1
 	
 	if guess == solution:
-		print("You found the secret word in", guesses, "tries :)")
+		print("\nYou found the secret word",
+			f"in {guesses}/{max_guesses} tries :)\n")
 		break
 
 	if guesses >= max_guesses:
-		print("No more tries lief, sorry :(")
+		print("\nNo more tries left, sorry :(\n")
 		break
