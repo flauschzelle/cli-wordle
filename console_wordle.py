0b96ddf82a7c148635163ccc35e8c09936ce9960
Kirstin Rohwer <mail@metakiki.net>
2022-02-21 15:41:27 +0100

    display guesses in bold text on green background

--- console_wordle.py
+++ console_wordle.py
@@ -7,15 +7,30 @@ solution = "wordl"
 guesses = 0
 max_guesses = 6
 
+BGREEN = '\033[42m'
+BYELLOW = '\033[43m'
+BBLACK = '\033[40m'
+
+BLACK = '\033[30m'
+WHITE = '\033[37m'
+
+ENDC = '\033[0m'
+BOLD = '\033[1m'
+
 print("Welcome to CONSOLE WORDLE!")
 print("Can you guess the secret word in", max_guesses, "or less tries?\n")
 
+def bold_colored_text(text: str, color: str) -> str:
+	ctext = f"{BOLD}{color}{text}{ENDC}"
+	return ctext
+	
 while guesses < max_guesses:
 
 	guess = str(input())
 	
-	print("\x1B[F\x1B[2K", end="") #overwrite input line after entering
-	print(guess)
+	print("\x1B[F\x1B[2K", end="") # overwrite input line after entering
+
+	print(bold_colored_text(guess, BGREEN))
 
 	guesses += 1
 	
