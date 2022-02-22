0876a33f4529d28c55b968cf5aa13460f8e3f750
Kirstin Rohwer <mail@metakiki.net>
2022-02-22 22:55:24 +0100

    fix some formatting

--- generate_word_list.py
+++ generate_word_list.py
@@ -9,13 +9,13 @@ word_len = 5
 fin = open("words_alpha.txt", "r")
 fout = open(f"words{word_len}.txt", "x")
 
-while(True):
-	#read next line
+while True:
+	# read next line:
 	line = fin.readline()
-	#if line is empty, you are done with all lines in the file
+	# if line is empty, you are done with all lines in the file
 	if not line:
 		break
-	#do things with the line
+	# do things with the line
 	word = line.strip()
 	if len(word) == word_len:
 		fout.write(f"{word}\n")
