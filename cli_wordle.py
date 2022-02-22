aa495c28f688d66720883b4ec1ab273eccb14770
Kirstin Rohwer <mail@metakiki.net>
2022-02-22 21:33:38 +0100

    display input letters in correct place, enforce word length

--- cli_wordle.py
+++ cli_wordle.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
-# A wordle implementation for unix terminals. 
+# A wordle implementation for unix command line.
 # This will probably not work correctly on Windows, sorry!
 
 import random
+from get_char import get_char
 
 # some parameters and defaults:
 
@@ -11,22 +12,25 @@ word_len = 5
 max_guesses = word_len + (word_len//4)
 guesses = 0
 guessed = []
-message = "Type a word and press ENTER to guess!"
-message_lines = 3
+message = "Type a word and\n press ENTER to guess!"
+message_lines = 4
 lines = max_guesses + message_lines
 
-# load the list of allowed words to guess:
-def load_words(word_len) -> list:
-	filename = "words" + str(word_len) + ".txt"
+
+def load_words(length) -> list:
+	# loads the list of allowed words to guess
+
+	filename = "words" + str(length) + ".txt"
 	file = open(filename, "r")
 	words = []
-	while(True):
+	while True:
 		line = file.readline()
 		if not line:
 			break
 		words.append(line.strip().upper())
 	file.close()
 	return words
+
 
 all_words = load_words(word_len)
 
@@ -35,7 +39,7 @@ all_words = load_words(word_len)
 pick_number = random.randint(0, len(all_words))
 solution = all_words[pick_number]
 
-# some colors etc for output formatting:
+# some colors etc. for output formatting:
 
 BGREEN = '\033[42m'
 BYELLOW = '\033[43m'
@@ -50,13 +54,15 @@ BOLD = '\033[1m'
 
 # helpful functions:
 
+
 def bold_colored_text(text: str, color: str) -> str:
-	ctext = f"{BOLD}{color} {text} {ENDC}"
+	ctext: str = f"{BOLD}{color} {text} {ENDC}"
 	return ctext
 
+
 def color_code_hints(text: str, pattern: str) -> str:
-	ctext = ""
-	hidden = []
+	ctext: str = ""
+	hidden: list = []
 
 	for i in range (len(pattern)):
 		if text[i] != pattern[i]:
@@ -72,6 +78,7 @@ def color_code_hints(text: str, pattern: str) -> str:
 			ctext += bold_colored_text(text[i], BGRAY)
 	return ctext
 
+
 def display_guesses(from_line: int):
 	for i in range (from_line, max_guesses):
 		if i < guesses:
@@ -79,10 +86,20 @@ def display_guesses(from_line: int):
 		else:
 			print(f" {BBLACK}{' '*3*word_len}{ENDC}")
 
+
+def display_input_char(ch: str):
+	# letters:
+	if ch.isalpha():
+		print(f" {ch.upper()} ", end="", flush=True)
+	# delete / backspace:
+	elif ch == '\x7f':
+		print("\b\b\b\x1B[0K", end="", flush=True)
+
+
 # the actual game starts here:
 
-print("\n Welcome to CONSOLE WORDLE!")
-print(" Guess the secret", word_len, "letter word in", max_guesses, "or less tries!\n")
+print("\n Welcome to COMMAND LINE WORDLE!\n")
+print(" Guess the", word_len, "letter word\n in", max_guesses, "or less tries!\n")
 
 # start with showing the empty grid:
 
@@ -95,19 +112,61 @@ print(f"\x1B[{str(lines)}F\x1B[2K", end="")
 
 while guesses < max_guesses:
 
-	# truncate input string and convert to uppercase:
-	guess = str(input(" "))[0:word_len].upper()
+	print(" ", end="", flush=True)
 
-	print("\x1B[1F\x1B[0J", end="") # overwrite input line after entering
+	current_input: str = ""
 
-	current_line = guesses
+	while len(current_input) <= word_len:
+		c: str = get_char()
 
-	if len(guess) == word_len and (guess in all_words):
-		message = "Type a word and press ENTER to guess!"
+		# in an empty line, only letters are allowed:
+		if len(current_input) == 0:
+			if c.isalpha():
+				current_input += c
+				display_input_char(c)
+			elif c == '\x7f' or c == '\n':
+				continue
+			else:
+				break
+
+		# in an incomplete line, backspace is also allowed:
+		elif len(current_input) < word_len:
+			if c.isalpha():
+				current_input += c
+			elif c == '\x7f':
+				current_input = current_input[0:-1]
+			elif c == '\n':
+				continue
+			else:
+				break
+			display_input_char(c)
+
+		# in a full line, only backspace or enter is allowed:
+		elif len(current_input) == word_len:
+			if c == '\x7f':
+				current_input = current_input[0:-1]
+				display_input_char(c)
+			elif c.isalpha():
+				continue
+			elif c == '\n':
+				break
+			else:
+				break
+
+	# save the guess:
+	guess = current_input.upper()
+
+	# overwrite input line after entering:
+	print("\x0D\x1B[0J", end="")
+
+	current_line: int = guesses
+
+	if guess in all_words:
+		message = "Type a word and\n press ENTER to guess!"
 		guesses += 1
 		guessed.append(guess)
 	else:
-		message = f"{guess} is not a valid word. Try again!"
+		message = f"{guess} is not a valid word.\n Try again!"
 
 	display_guesses(current_line)
 	
@@ -124,4 +183,4 @@ while guesses < max_guesses:
 	# set the cursor to start the input on the next empty line:
 	print(f"\x1B[{str(lines-guesses)}F\x1B[2K", end="")
 
-print(f" Random word number {pick_number} of {len(all_words)}")
+print(f" (Random word number {pick_number} of {len(all_words)})\n")
---
+++ get_char.py
@@ -0,0 +1,29 @@
+# taken from here: https://stackoverflow.com/a/36974338
+
+def get_char():
+    # figure out which function to use once, and store it in _func
+    if "_func" not in get_char.__dict__:
+        try:
+            # for Windows-based systems
+            import msvcrt # If successful, we are on Windows
+            get_char._func = msvcrt.getch
+
+        except ImportError:
+            # for POSIX-based systems (with termios & tty support)
+            import tty, sys, termios  # raises ImportError if unsupported
+
+            def _tty_read():
+                fd = sys.stdin.fileno()
+                old_settings = termios.tcgetattr(fd)
+
+                try:
+                    tty.setcbreak(fd)
+                    answer = sys.stdin.read(1)
+                finally:
+                    termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+
+                return answer
+
+            get_char._func = _tty_read
+
+    return get_char._func()
